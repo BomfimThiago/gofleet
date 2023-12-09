56cd6184bdc9bcd127ac5a65600fe8a46d3ff84d
Thiago Bomfim <bomfimdev@gmail.com>
2023-12-09 16:08:14 -0500

    submission-card component

--- zenduforms/src/app/submission/submission-card/submission-card.component.ts
+++ zenduforms/src/app/submission/submission-card/submission-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BadgeComponent } from '../badge/badge.component';
 
 @Component({
   selector: 'app-submission-card',
   standalone: true,
-  imports: [],
+  imports: [BadgeComponent],
   templateUrl: './submission-card.component.html',
   styleUrl: './submission-card.component.css'
 })
